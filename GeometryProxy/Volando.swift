df858cb7dae3c90a98cbd146a72a11348239a825
AndresMzaDev <andes_nmeza@hotmail.com>
2023-12-10 01:03:39 -0600

    firt commit

---
+++ GeometryProxy/Volando.swift
@@ -0,0 +1,20 @@
+//
+//  Volando.swift
+//  GeometryProxy
+//
+//  Created by andres meza on 2/2/22.
+//
+
+import SwiftUI
+
+struct Volando: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Volando_Previews: PreviewProvider {
+    static var previews: some View {
+        Volando()
+    }
+}
