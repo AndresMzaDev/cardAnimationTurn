e4602cc00abff4e64193ec4209a6133f8374a238
AndresMzaDev <andes_nmeza@hotmail.com>
2023-12-10 01:04:28 -0600

    git unit

--- GeometryProxy/ContentView.swift
+++ GeometryProxy/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+        GeometryReader{ proxy in
+            
+            VStack(spacing: 0){
+                HStack(spacing: 0){
+                    Rectangle()
+                        .foregroundColor(Color.green)
+                        .overlay(Text("1"))
+                        .frame(width: proxy.size.width * 0.33, height: proxy.size.height/2)
+                    Rectangle()
+                        .foregroundColor(Color.orange)
+                        .overlay(Text("2"))
+                        .frame(width: proxy.size.width * 0.67, height: proxy.size.height/2)
+                }
+                Rectangle()
+                    .foregroundColor(Color.purple)
+                    .overlay(Text("3"))
+                    .frame(width: proxy.size.width, height: proxy.size.height * 0.33)
+            }
+            
+         
+        }.background(Color.red)
+        }
+        
+        
     }
 }
 
--- GeometryProxy/Volando.swift
+++ GeometryProxy/Volando.swift
@@ -7,9 +7,48 @@
 
 import SwiftUI
 
+let arrayOfNames = [
+    "A",
+    "B" ,
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H"
+]
+
+
 struct Volando: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false){
+            VStack{
+                ForEach(arrayOfNames, id: \.self) {
+                    name in GeometryReader {
+                        proxy in
+                        VStack{
+                            
+                            Text("\(name)")
+                                .frame(width: 370, height: 200)
+                                .background(Color.orange   )
+                                .cornerRadius(20)
+                            Spacer()
+                        }
+                        .shadow(color: .gray, radius: 10, x: 0, y: 0)
+                        .rotation3DEffect(Angle (degrees: Double(proxy.frame(in: .global).minY)-47), axis: (x: 0.0    , y: 10.0, z: 0.0))
+                    }
+                    .frame(width: 400, height: 200)
+                }
+            }.padding(.trailing)
+        }.padding(.horizontal)
+        
+        
     }
 }
 
